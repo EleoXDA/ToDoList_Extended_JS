f919b3e6d5465db3c317c465da5e8ef0b347843d
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-09-18 01:25:10 +0200

    ADD uniqueness function

--- assets/js/item_uniqueness.js
+++ assets/js/item_uniqueness.js
@@ -1,2 +1,15 @@
-function itemUniqueness(){
+function itemUniqueness(array,value) {
+  let error = 0;
+
+  array.forEach(element => {
+    if(element === value) {
+      error = 1;
+    }
+  });
+
+  if(error == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
