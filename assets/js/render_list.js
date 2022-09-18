5f596af4326b06445edcaa2f903299fdde0a81b0
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-09-18 09:06:34 +0200

    add edit button

--- assets/js/render_list.js
+++ assets/js/render_list.js
@@ -3,7 +3,8 @@ function renderList(array){
 
   array.forEach((element, index) => {
     listElement += `<li class="list-group-item d-flex justify-content-between">
-    <span>${element}</span> 
+    <span>${element}</span>
+    <button class="btn btn-success btn-sm">Edit</button>
     <button class="btn btn-danger btn-sm" onclick="deleteToDoItem('${index}')">Delete</button>
     </li>`;
   });
