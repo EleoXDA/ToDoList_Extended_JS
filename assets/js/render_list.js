88f216e35b85a8618a26daf83a83e5f3d0e56859
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-09-18 09:06:48 +0200

    wrap buttons in container to move them to the edge

--- assets/js/render_list.js
+++ assets/js/render_list.js
@@ -4,8 +4,10 @@ function renderList(array){
   array.forEach((element, index) => {
     listElement += `<li class="list-group-item d-flex justify-content-between">
     <span>${element}</span>
+    <div>
     <button class="btn btn-success btn-sm">Edit</button>
     <button class="btn btn-danger btn-sm" onclick="deleteToDoItem('${index}')">Delete</button>
+    </div>
     </li>`;
   });
   listElement += `</ul>`;
